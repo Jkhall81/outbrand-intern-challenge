473084fdda1f2a3f6582098b41cb9b44ee7fb466
Jkhall81 <jason.kei.hall@gmail.com>
2024-02-04 15:35:26 -0700

    added readme and license.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const admin = require("firebase-admin");
+const bcrypt = require("bcryptjs");
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require("../frontend/outbrand-e10a3-firebase-adminsdk-5tdmz-bcf2b08ce0.json");
@@ -18,56 +19,67 @@ app.use(cors());
 // Define your API routes here
 
 // User Registration
-app.post("/api/user/register", (req, res) => {
-  // email, fullName, password
-  const data = req.body;
-  const usersRef = admin.database().ref("users");
-  const userData = {
-    fullName: data.fullName,
-    email: data.email,
-    password: data.password,
-  };
+app.post("/api/user/register", async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
 
-  // pushing the data so i don't overwrite existing data
-  const newUsersRef = usersRef.push();
-  newUsersRef
-    .set(userData)
-    .then(() => {
-      console.log("Data saved successfully");
-      res
-        .status(200)
-        .json({ message: "User registered successfully", userData });
-    })
-    .catch((error) => {
-      console.error("Error saving data:", error);
-      res.status(500).json({ message: "Error registering user", error });
-    });
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const usersRef = admin.database().ref("users");
+    const userData = {
+      fullName: fullName,
+      email: email,
+      password: hashedPassword, // Store the hashed password
+    };
+
+    const newUsersRef = usersRef.push();
+    newUsersRef
+      .set(userData)
+      .then(() => {
+        console.log("Data saved successfully");
+        res.status(200).json({
+          message: "User registered successfully",
+          userData: { fullName, email },
+        });
+      })
+      .catch((error) => {
+        console.error("Error saving data:", error);
+        res.status(500).json({ message: "Error registering user", error });
+      });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Error registering user", error });
+  }
 });
 
 // User Login
 app.post("/api/user/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const usersRef = admin.database().ref("users");
   try {
-    const snapshot = await usersRef.orderByChild("email").once("value");
+    const { email, password } = req.body;
 
-    let userFound = false;
+    const usersRef = admin.database().ref("users");
+    const snapshot = await usersRef
+      .orderByChild("email")
+      .equalTo(email)
+      .once("value");
 
-    snapshot.forEach((childSnapshot) => {
-      const user = childSnapshot.val();
+    if (snapshot.exists()) {
+      const userId = Object.keys(snapshot.val())[0];
+      const user = snapshot.val()[userId];
 
-      if (user.email === email && user.password === password) {
-        userFound = true;
+      // Compare the hashed password
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) {
+        return res
+          .status(200)
+          .json({ message: "User logged in successfully", user: { email } });
+      } else {
+        return res.status(401).json({ message: "Invalid email or password" });
       }
-    });
-
-    if (userFound) {
-      return res
-        .status(200)
-        .json({ message: "User logged in successfully", user: { email } });
     } else {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
     console.error("Error checking user:", error);
